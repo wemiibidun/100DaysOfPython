30d34096a69376f415b088aebf24f999fdd97762
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-02-16 06:48:46 -0700

    Create treasure_island.py

---
+++ treasure_island.py
@@ -0,0 +1,48 @@
+#This is a treasure island game. All a user needs to do is to play & win by following the instructions
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+user = input("To start the game, choose either left or right? \n")
+
+user = user.lower()
+
+if user == "left":
+  user = input("Choose between to swim or to wait? \n")
+  if user == "swim":
+    user = input("Which colour of door will you like to open? \n")
+    if user == "yellow":
+      print("Yayyy, you win")
+    elif user == "blue":
+      print("You have been beaten by beasts. Game Over")
+    elif user == "red":
+      print("Oops, you have been burned by fire. Game over")
+    else:
+      print("Game Over.")  
+  else:
+     print("You have been attacked by trout. Game Over.")       
+else:
+  print("Oops, you fell into a hole. Game is over")  
