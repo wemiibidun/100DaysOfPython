a3e031b7c3d834d4655016b76b3fefd6eb267866
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-02-23 13:36:46 -0700

    Update day6_reeborgs_maze_challenge.py

--- day6_reeborgs_maze_challenge.py
+++ day6_reeborgs_maze_challenge.py
@@ -1,5 +1,23 @@
-**Direct link to this Github project and my Blog Page Documentation:**
+#This challenge was aimed at learning the use of while loops
 
-[First_Webpage](https://wemiibidun.github.io/my_first_webpage/)
+#Reeborg's world was used to practice while loos
 
-[Code With Me](http://www.wemiibidun.com/search/label/coding)
+#The maze project link can be found here --> https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+#My solution to the maze challenge👇
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+    
+while not at_goal():
+      if right_is_clear():
+            turn_right()
+            move()
+      elif front_is_clear():
+        move()
+      else:
+        turn_left()
+            
