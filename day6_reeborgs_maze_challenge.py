2302f12fc4bf89ffc8da8205f3987ecb7489ff4c
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-02-23 13:33:46 -0700

    Update day6_reeborgs_maze_challenge.py

--- day6_reeborgs_maze_challenge.py
+++ day6_reeborgs_maze_challenge.py
@@ -1,1 +1,5 @@
+**Direct link to this Github project and my Blog Page Documentation:**
 
+[First_Webpage](https://wemiibidun.github.io/my_first_webpage/)
+
+[Code With Me](http://www.wemiibidun.com/search/label/coding)
